734cec75978706dbd8b5b2ba3cb865fa8cfbd7d7
zengrui <772701414@qq.com>
2021-06-02 17:04:04 +0800

    增加common通用模块

---
+++ common/src/main/java/common/IErrorCode.java
@@ -0,0 +1,9 @@
+package common;
+
+/**
+ * 封装API的错误码
+ */
+public interface IErrorCode {
+    long getCode();
+    String getMessage();
+}
---
+++ common/src/main/java/log/domain/UserDto.java
@@ -0,0 +1,22 @@
+package log.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * 登录用户信息
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+public class UserDto {
+    private Long id;
+    private String username;
+    private String password;
+    private Integer status;
+    private String clientId;
+    private List<String> roles;
+}
